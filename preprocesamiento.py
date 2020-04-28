0f16c42ef40a529f523f65123af6068e28880bca
Alejandro Clavera <alejandroclavera@MacBook-Pro-de-Alejandro.local>
2020-04-28 21:48:57 +0200

    Se implementa la lectura de las muestras.

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -1,1 +1,24 @@
-#preprocesamiento
+import csv
+
+def load_locations(path):
+   with open(path, 'r') as csv_file:
+      csv_reader = csv.DictReader(csv_file)
+      next(csv_reader)
+      locations = {}
+      for row in csv_reader:
+         locations[row['Geo_Location']] = {}
+   return locations
+
+def open_csv(path):
+   with open(path, 'r') as csv_file:
+      csv_reader = csv.DictReader(csv_file)
+      next(csv_reader)
+      samples = load_locations(path)
+      for row in csv_reader:
+         samples[row['Geo_Location']][row['Accession']] = {'Length':row['Length']}      
+   return samples
+                  
+samples = open_csv('sequences.csv')
+
+
+
