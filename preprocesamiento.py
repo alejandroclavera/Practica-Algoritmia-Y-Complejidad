be427713e9bf0cdae99bcd5e8358f681931452b0
sergi <sergi.puigpinos@hotmail.com>
2020-05-02 20:10:18 +0200

    Update preprocesamiento.py

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -1,1 +1,31 @@
+<<<<<<< Updated upstream
 #preprocesamiento
+=======
+import csv
+
+def load_regions(path):
+   with open(path, 'r') as csv_file:
+      csv_reader = csv.DictReader(csv_file)
+      next(csv_reader)
+      locations = {}
+      for row in csv_reader:
+         locations[row['Geo_Location']] = {}
+   return locations
+
+def load_samples_of_csv(path):
+   with open(path, 'r') as csv_file:
+      csv_reader = csv.DictReader(csv_file)
+      next(csv_reader)
+      samples = load_regions(path)
+      for row in csv_reader:
+         samples[row['Geo_Location']][row['Accession']] = {'Length':int(row['Length'])}      
+   return samples
+
+#test2
+
+samples = load_samples_of_csv('sequences.csv')
+
+
+
+
+>>>>>>> Stashed changes
