9a3592f28aef591e57083f1226d020ce9141d100
Jordi Lazo <jordilazo94@gmail.com>
2020-05-02 11:01:46 +0200

    test quick_sort

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -1,4 +1,6 @@
 import csv
+from collections import OrderedDict
+from operator import getitem
 
 def load_locations(path):
    with open(path, 'r') as csv_file:
@@ -17,8 +19,46 @@ def open_csv(path):
       for row in csv_reader:
          samples[row['Geo_Location']][row['Accession']] = {'Length':row['Length']}      
    return samples
-                  
+
+def quick_sort(sequence):
+   less = []
+   pivotlist = []
+   more = []
+   if len(sequence) <=1:
+      return sequence
+   else:
+      pivot = sequence[0]
+      for i in sequence:
+         if i < pivot:
+            less.append(i)
+         elif i > pivot:
+            more.append(i)
+         else:
+            pivotlist.append(i)
+      less = quick_sort(less)
+      more = quick_sort(more)
+      return less + pivotlist + more
+
+def sort_dictionary(samples):
+   print(samples)
+   #samples.sort(key=operator.itemgetter(['Length']))
+   #samples_sorted = {k: v for k, v in sorted(samples.items(), key=lambda x:x['Length'])}
+   #print(samples)
+   print(samples['Length'].values())
+   #a = OrderedDict(sorted(samples.items(), key = lambda x: getitem(int(x[1]),'Length')))
+   #print(a)
+
+      #print(i, samples[i])
 samples = open_csv('sequences.csv')
+
+print(list(samples['Hong Kong'].keys()))
+#a = [4,65,2,0,99,83,678,23]
+print((list(samples['Hong Kong'].values())))
+#x = open_csv('sequences.csv')
+print(sort_dictionary(samples))
+#print(samples['Spain']['MT359865']['Length'])
+#print(samples['Hong Kong'])
+
 
 
 
