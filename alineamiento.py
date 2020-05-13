2533a9a9e125f07cbad2553767f1b944e6514084
Alejandro Clavera <alejandroclavera@MacBook-Pro-de-Alejandro.local>
2020-05-13 11:23:51 +0200

    Algoritmo de Needleman–Wunsch es implementado con C.

--- alineamiento.py
+++ alineamiento.py
@@ -4,11 +4,12 @@
 import requests
 import os
 import preprocesamiento
+import ctypes
 
 def get_arn_string(content):
-   content = content.decode('utf-8')
-   arn_string = content[content.find('\n') + 1:]
-   return arn_string
+    content = content.decode('utf-8')
+    arn_string = content[content.find('\n') + 1:]
+    return arn_string
 
 def load_arn_samples(csv_path):
    samples = preprocesamiento.get_median_samples_of_csv(csv_path)
@@ -17,6 +18,7 @@ def load_arn_samples(csv_path):
       os.mkdir('samples/')
    for sample in samples:
       id = sample[0]
+      samples_id_list.append(id)
       sample_path = 'samples/{0}.fasta'.format(id)
       has_error = False
       error_list = []
@@ -39,92 +41,37 @@ def load_arn_samples(csv_path):
       print('Se han cargado las muestras correctamente y no hay errores en las longitudes')
    return samples_id_list
 
-load_arn_samples('sequences.csv')  
-
-
-
 #ALGORITM: Needleman–Wunsch
-similarityMatrix = [[10,-1,-3,-4],[-1,7,-5,-3],[-3,-5,9,0],[-4,-3,0,8]]
-gapPenalty = -5 
-charIndx = {
-    "A":0,
-    "G":1,
-    "C":2,
-    "T":3,
-}
-
-def calcSimil(a,b):
-    if a == '-' or b == '-':
-        return gapPenalty
-    else:
-        return similarityMatrix[charIndx[a]][charIndx[b]]
-
 def calcNeedlemanScore(seq1, seq2):
-    M = []
+    needlemanScore = ctypes.CDLL('library/alineamiento.so')
+    needlemanScore.calc_needleman_score.argtypes = (ctypes.POINTER(ctypes.c_char), ctypes.c_int,ctypes.POINTER(ctypes.c_char), ctypes.c_int,)
+    needlemanScore.calc_needleman_score.restype = ctypes.c_int 
     n = len(seq1)
     m = len(seq2)
-    M = [[0 for x in range(n+1)] for y in range(m+1)] 
-    #Base cases
-    for i in range(m+1):
-        M[i][0] = gapPenalty*i
-    for j in range(n+1):
-        M[0][j] = gapPenalty*j
-    #Calculate diagonal cost
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            Match = M[i-1][j-1] + calcSimil(seq1[j-1],seq2[i-1])
-            Delete = M[i-1][j] + gapPenalty
-            Insert = M[i][j-1] + gapPenalty
-            M[i][j] = max(Match,Delete,Insert)
-    #Align words
-    align1 = ""
-    align2 = ""
-    i = m
-    j = n
-    while i > 0 and j > 0:
-        if M[i][j] == M[i-1][j-1] + calcSimil(seq1[j-1], seq2[i-1]):
-            align1 += seq1[j-1]
-            align2 += seq2[i-1]
-            i -= 1
-            j -= 1
-        elif M[i][j] == M[i][j-1] + gapPenalty:
-            align1 += seq1[j-1]
-            align2 += '-'
-            j -= 1
-        elif M[i][j] == M[i-1][j] + gapPenalty:
-            align1 += '-'
-            align2 += seq2[i-1]
-            i -= 1
-    # Finish tracing up to the top left cell
-    while j > 0:
-        align1 += seq1[j-1]
-        align2 += '-'
-        j -= 1
-    while i > 0:
-        align1 += '-'
-        align2 += seq2[i-1]
-        i -= 1
-    #Invert words because we started from the bottom left (debug only)
-    '''
-    align1 = align1[::-1]
-    align2 = align2[::-1] 
-    '''
-    #Calculate score
-    score = 0
-    i = min(len(align1),len(align2))-1
-    while i >= 0:
-        score += calcSimil(align1[i],align2[i])
-        i -= 1
-    return score
+    seq1_array = ctypes.c_char * n
+    seq2_array = ctypes.c_char * m
+    n = ctypes.c_int(n)
+    m = ctypes.c_int(m) 
+    return needlemanScore.calc_needleman_score(seq1_array(*seq1.encode()),n,seq2_array(*seq2.encode()),m)
 
-#Debug only
-'''
-def printMatrix(M):
-    for r in M:
-        for c in r:
-            print(c, end=" ")
-        print()
-'''
-#More score -> more similar
-print(calcNeedlemanScore("ATATAGC","A"))
+samples = load_arn_samples('sequences.csv')
+def get_arn_sample(sample_id):
+   try:
+      sample_file = open('samples/{0}.fasta'.format(sample_id), 'r')
+      sample = ''
+      for line in sample_file:
+         sample += line.split('\n')[0]
+      return sample
+   except:
+      print('Error al cargar muestra')
+     
+arn_str1 = get_arn_sample(samples[0])
+arn_str12 = get_arn_sample(samples[1])
+#print(calcNeedlemanScore(arn_str1,arn_str12))
+#print(calcNeedlemanScore("ATATAGC","ATATGC"))
+print(calcNeedlemanScore("GCATG-CA","G-ATTACA"))
+
+
+
+
 
